24c6057cb5ae4459f6b75ce4d8180b09b79d7018
Anderson Alves <contatodeanderson@gmail.com>
2024-02-11 22:22:19 -0300

    add log

--- src/main/kotlin/com/kotlin/blog/utils/JsonUtils.kt
+++ src/main/kotlin/com/kotlin/blog/utils/JsonUtils.kt
@@ -1,9 +1,16 @@
 import com.google.gson.Gson
+import org.slf4j.LoggerFactory
 
 object JsonUtils {
     private val gson = Gson()
+    private val logger = LoggerFactory.getLogger(JsonUtils::class.java)
 
     fun toJson(obj: Any): String {
-        return gson.toJson(obj)
+        return try {
+            gson.toJson(obj)
+        } catch (e: Exception) {
+            logger.error("Erro ao converter objeto para JSON: {}", e.message)
+            ""
+        }
     }
 }
